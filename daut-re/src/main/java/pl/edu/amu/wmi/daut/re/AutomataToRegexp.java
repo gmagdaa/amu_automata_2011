59f9b469789bbecd51c1a7af3f821583a3eea755
unknown <Shin@Asus.(none)>
2012-01-22 20:30:25 +0100

    Poprawki

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AutomataToRegexp.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AutomataToRegexp.java
@@ -1,9 +1,9 @@
+package pl.edu.amu.wmi.daut.re;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.Stack;
 
 /**
